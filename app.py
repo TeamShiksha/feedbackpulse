4bac0543d05ec73bfa74a9c4191498f1bba76306
aps08 <anoopprsingh.social@gmail.com>
2024-09-24 01:13:20 +0530

    responsive in all major screens

--- app.py
+++ app.py
@@ -2,18 +2,12 @@
 feedbackpulse: Web app starts here.
 """
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    """
-    Just redirect to /home where user can find the landing page.
-    """
-    return redirect("/home")
 
-@app.route("/home")
+@app.route("/")
 def home():
     """
     Landing page of the app with GitHub OAuth login. This is to
